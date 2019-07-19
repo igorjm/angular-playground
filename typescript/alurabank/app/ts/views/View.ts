1574886e28693d6a5d58989d56a863d98b7607e3
Igor Melo <igorjmelo4@gmail.com>
2019-07-18 23:01:44 -0300

    adding jquery and refactoring

---
+++ typescript/alurabank/app/ts/views/View.ts
@@ -0,0 +1,17 @@
+declare var $: any;
+
+abstract class View<T> {
+
+    protected _elemento: JQuery;
+
+    constructor(selector: string) {
+        this._elemento = $(selector);
+    }
+
+    update(model: T) {
+        this._elemento.html(this.template(model));
+    }
+
+    abstract template(model: T): string;
+
+}
