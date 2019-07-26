cf995529208e5594f1fb132902f3b77f929179a3
igorjm <igorjmelo4@gmail.com>
2019-07-26 16:40:21 -0300

    adding components

--- 4.angular/alurapic/src/app/app.component.ts
+++ 4.angular/alurapic/src/app/app.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-export class AppComponent {
-  photos: any[] = [];
-
-  constructor(photoService: PhotoService) {
-    photoService
-      .listFromUser('flavio')
-      .subscribe(photos => this.photos = photos);
-  }
-}
+export class AppComponent {}
