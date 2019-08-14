30550da18e95ae458fcf73be257b5d8f7527fc1d
Igor Melo <igorjmelo4@gmail.com>
2019-08-13 23:28:24 -0300

    finishing directives and modules

---
+++ 1.basics/1.9-modulos/js/services/operadorasAPIService.js
@@ -0,0 +1,7 @@
+//SERVICO FUNCAO CONSTRUTORA
+
+angular.module("listaTelefonica").service("operadorasAPI", function($http, config) {
+    this.getOperadoras = function() {
+        return $http.get(config.baseUrl + "/operadoras");
+    };
+});
